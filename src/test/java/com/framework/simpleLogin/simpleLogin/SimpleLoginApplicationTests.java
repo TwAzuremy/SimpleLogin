33f1bafd38076f9ffa0f5bf08128f3977373252e
TwAzuremy <958559114@qq.com>
2025-01-24 15:16:40 +0800

    init

---
+++ src/test/java/com/framework/simpleLogin/simpleLogin/SimpleLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framework.simpleLogin.simpleLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
