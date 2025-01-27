14256ab6ce7b35383f82d6adc7fa767ef6dab5ff
TwAzuremy <958559114@qq.com>
2025-01-28 00:28:53 +0800

    Remove the interceptor and use a filter instead

--- src/main/java/com/framework/simpleLogin/config/WebConfig.java
+++ src/main/java/com/framework/simpleLogin/config/WebConfig.java
@@ -1,15 +1,19 @@
 package com.framework.simpleLogin.config;
 
 import com.framework.simpleLogin.interceptor.UserInterceptor;
+import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Resource
+    private UserInterceptor userInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new UserInterceptor())
+        registry.addInterceptor(userInterceptor)
                 .addPathPatterns("/user/login");
     }
 }
---
+++ src/main/java/com/framework/simpleLogin/filter/UserFilter.java
@@ -0,0 +1,58 @@
+package com.framework.simpleLogin.filter;
+
+import com.framework.simpleLogin.exception.InvalidJwtException;
+import com.framework.simpleLogin.service.RedisService;
+import com.framework.simpleLogin.utils.CACHE_NAME;
+import com.framework.simpleLogin.utils.JwtUtils;
+import com.framework.simpleLogin.utils.SimpleUtils;
+import jakarta.annotation.Resource;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.annotation.Order;
+
+import java.io.IOException;
+
+@Order(1)
+@WebFilter(urlPatterns = {"/user/login", "/user/verify"})
+public class UserFilter implements Filter {
+    private final JwtUtils jwtUtils = new JwtUtils();
+
+    @Resource
+    private RedisService redisService;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String token = ((HttpServletRequest) servletRequest).getHeader("Authorization");
+
+        if (SimpleUtils.stringIsEmpty(token) || !token.startsWith(SimpleUtils.authorizationPrefix)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        token = token.substring(SimpleUtils.authorizationPrefix.length());
+
+        try {
+            String email = jwtUtils.getClaims(token).get("email").toString();
+            String isExists = (String) redisService.get(CACHE_NAME.USER + ":token:" + email);
+
+            if (SimpleUtils.stringIsEmpty(isExists) || !jwtUtils.validateToken(token)) {
+                throw new InvalidJwtException("The JWT token is invalid.");
+            }
+        } catch (RuntimeException e) {
+            throw new InvalidJwtException("The JWT token is invalid.");
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
