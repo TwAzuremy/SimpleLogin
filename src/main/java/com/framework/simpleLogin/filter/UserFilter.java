6ba42a9dce4760d7b6f72256d2cb5d3757464fda
TwAzuremy <958559114@qq.com>
2025-01-30 15:42:00 +0800

    Relocate the verification code to the interceptor.

--- src/main/java/com/framework/simpleLogin/filter/UserFilter.java
+++ src/main/java/com/framework/simpleLogin/filter/UserFilter.java
@@ -1,30 +1,14 @@
 package com.framework.simpleLogin.filter;
 
-import com.framework.simpleLogin.exception.InvalidJwtException;
-import com.framework.simpleLogin.service.RedisService;
-import com.framework.simpleLogin.utils.CACHE_NAME;
-import com.framework.simpleLogin.utils.JwtUtils;
-import com.framework.simpleLogin.utils.SimpleUtils;
-import jakarta.annotation.Resource;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.annotation.Order;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Set;
 
 @Order(1)
 @WebFilter(urlPatterns = {"/users/*"})
 public class UserFilter implements Filter {
-    private static final Set<String> ALLOWED_PATHS = Set.of("/users/register");
-    private final JwtUtils jwtUtils = new JwtUtils();
-
-    @Resource
-    private RedisService redisService;
-
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -32,33 +16,6 @@ public class UserFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        String path = request.getRequestURI().substring(request.getContextPath().length()).replaceAll("/+$", "");
-
-        if (ALLOWED_PATHS.contains(path)) {
-            filterChain.doFilter(servletRequest, servletResponse);
-            return;
-        }
-
-        String token = request.getHeader("Authorization");
-
-        if (SimpleUtils.stringIsEmpty(token) || !token.startsWith(SimpleUtils.authorizationPrefix)) {
-            filterChain.doFilter(servletRequest, servletResponse);
-            return;
-        }
-
-        try {
-            token = token.substring(SimpleUtils.authorizationPrefix.length());
-
-            String email = jwtUtils.getClaims(token).get("email").toString();
-            String cacheToken = (String) redisService.get(CACHE_NAME.USER + ":token:" + email);
-
-            if (SimpleUtils.stringIsEmpty(cacheToken) || !jwtUtils.validateToken(token) || !cacheToken.equals(token)) {
-                throw new InvalidJwtException("The JWT token is invalid.");
-            }
-        } catch (RuntimeException e) {
-            throw new InvalidJwtException("The JWT token is invalid.");
-        }
 
         filterChain.doFilter(servletRequest, servletResponse);
     }
--- src/main/java/com/framework/simpleLogin/interceptor/UserInterceptor.java
+++ src/main/java/com/framework/simpleLogin/interceptor/UserInterceptor.java
@@ -1,17 +1,68 @@
 package com.framework.simpleLogin.interceptor;
 
+import com.framework.simpleLogin.exception.InvalidJwtException;
+import com.framework.simpleLogin.listener.JpaValidationEvent;
+import com.framework.simpleLogin.service.RedisService;
+import com.framework.simpleLogin.utils.CACHE_NAME;
+import com.framework.simpleLogin.utils.JwtUtils;
+import com.framework.simpleLogin.utils.SimpleUtils;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Set;
+
 @Component
 public class UserInterceptor implements HandlerInterceptor {
+    private static final Set<String> ALLOWED_PATHS = Set.of("/users/register");
+    private final JwtUtils jwtUtils = new JwtUtils();
+
+    @Resource
+    private ApplicationEventPublisher eventPublisher;
+
+    @Resource
+    private RedisService redisService;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String path = request.getRequestURI().substring(request.getContextPath().length()).replaceAll("/+$", "");
+
+        if (ALLOWED_PATHS.contains(path)) {
+            return true;
+        }
+
+        String token = request.getHeader("Authorization");
+
+        if (SimpleUtils.stringIsEmpty(token) || !token.startsWith(SimpleUtils.authorizationPrefix)) {
+            return true;
+        }
+
+        try {
+            token = token.substring(SimpleUtils.authorizationPrefix.length());
+
+            String email = jwtUtils.getClaims(token).get("email").toString();
+            String cacheToken = (String) redisService.get(CACHE_NAME.USER + ":token:" + email);
+
+            if (SimpleUtils.stringIsEmpty(cacheToken) || !jwtUtils.validateToken(token) || !cacheToken.equals(token)) {
+                eventPublisher.publishEvent(
+                        new JpaValidationEvent(this, "User '" + email + "', JPA validation failed.", false)
+                );
+
+                throw new InvalidJwtException("The JWT token is invalid.");
+            }
+
+            eventPublisher.publishEvent(
+                    new JpaValidationEvent(this, "User '" + email + "', JPA validation successful.", true)
+            );
+        } catch (RuntimeException e) {
+            throw new InvalidJwtException("The JWT token is invalid.");
+        }
+
         return true;
     }
 
