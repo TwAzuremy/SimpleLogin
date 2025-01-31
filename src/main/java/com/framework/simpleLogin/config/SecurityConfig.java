78fb02febc4b5793c5e31c91fdd6d3dc88be57ba
TwAzuremy <958559114@qq.com>
2025-02-01 02:39:15 +0800

    Remove unused items.

--- src/main/java/com/framework/simpleLogin/config/SecurityConfig.java
+++ src/main/java/com/framework/simpleLogin/config/SecurityConfig.java
@@ -2,11 +2,9 @@ package com.framework.simpleLogin.config;
 
 import com.framework.simpleLogin.filter.JwtAuthenticationTokenFilter;
 import com.framework.simpleLogin.utils.Encryption;
-import com.framework.simpleLogin.utils.ResponseEntity;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,7 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
--- src/main/java/com/framework/simpleLogin/filter/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/framework/simpleLogin/filter/JwtAuthenticationTokenFilter.java
@@ -9,20 +9,15 @@ import com.framework.simpleLogin.utils.RedisUtil;
 import jakarta.annotation.Resource;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
 
 @Component
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
