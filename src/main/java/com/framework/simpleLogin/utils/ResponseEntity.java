cbb324836196fbac8b19c83863a16f5b0549c7f5
TwAzuremy <958559114@qq.com>
2025-02-02 02:04:49 +0800

    Added added response header.

--- src/main/java/com/framework/simpleLogin/utils/ResponseEntity.java
+++ src/main/java/com/framework/simpleLogin/utils/ResponseEntity.java
@@ -1,14 +1,14 @@
 package com.framework.simpleLogin.utils;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.framework.simpleLogin.serializer.HttpStatusSerializer;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
-@AllArgsConstructor
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -18,9 +18,24 @@ public class ResponseEntity<T> {
     private String message;
     private T data;
 
+    @JsonIgnore
+    private HttpHeaders headers = new HttpHeaders();
+
+    public ResponseEntity(HttpStatus status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
     public ResponseEntity(HttpStatus status, T data) {
         this.status = status;
         this.message = status.getReasonPhrase();
         this.data = data;
+    }
+
+    public ResponseEntity<?> addHeader(String name, String value) {
+        this.headers.add(name, value);
+
+        return this;
     }
 }
