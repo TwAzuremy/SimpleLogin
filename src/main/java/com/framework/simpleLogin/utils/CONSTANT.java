0b56e77649fd4e767fae9dae27b2411c8563003c
TwAzuremy <958559114@qq.com>
2025-02-02 02:04:22 +0800

    Adjusted the structure of the constant class, added an external configuration file.

--- src/main/java/com/framework/simpleLogin/utils/CONSTANT.java
+++ src/main/java/com/framework/simpleLogin/utils/CONSTANT.java
@@ -1,7 +1,13 @@
 package com.framework.simpleLogin.utils;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Component
+@PropertySource("classpath:constant.properties")
 public final class CONSTANT {
     public static final class CACHE_NAME {
         public static final String CAPTCHA = "captcha";
@@ -17,19 +23,80 @@ public final class CONSTANT {
         public static final String API_DEBOUNCE = API + ":debounce";
     }
 
+    @Component
     public static final class CACHE_EXPIRATION_TIME {
-        public static final long USER_TOKEN = 7 * 24 * 60 * 60 * 1000;
-        public static final long USER_CACHE = 7 * 24 * 60 * 60 * 1000;
-        public static final long LOCK_TIME = 5 * 60 * 1000;
+        /*
+         * Since the default values used in the annotation are used here,
+         * it is not possible to assign values dynamically via get.
+         */
         public static final long API_DEBOUNCE_TIME = 60 * 1000;
-        public static final long CAPTCHA = 30 * 60 * 1000;
+
+        public static long USER_CACHE;
+        public static long USER_TOKEN;
+        public static long API_DEBOUNCE;
+        public static long ACCOUNT_LOCKED;
+        public static long CAPTCHA;
+
+        public CACHE_EXPIRATION_TIME(
+                @Value("${cache.expiration-time.user-cache}") long USER_CACHE,
+                @Value("${cache.expiration-time.user-token}") long USER_TOKEN,
+                @Value("${cache.expiration-time.api-debounce}") long API_DEBOUNCE,
+                @Value("${cache.expiration-time.account-locked}") long ACCOUNT_LOCKED,
+                @Value("${cache.expiration-time.captcha}") long CAPTCHA
+        ) {
+            CACHE_EXPIRATION_TIME.USER_CACHE = USER_CACHE;
+            CACHE_EXPIRATION_TIME.USER_TOKEN = USER_TOKEN;
+            CACHE_EXPIRATION_TIME.API_DEBOUNCE = API_DEBOUNCE;
+            CACHE_EXPIRATION_TIME.ACCOUNT_LOCKED = ACCOUNT_LOCKED;
+            CACHE_EXPIRATION_TIME.CAPTCHA = CAPTCHA;
+        }
+
+        public static long get(String key) {
+            return switch (key) {
+                case "user-cache" -> USER_CACHE;
+                case "user-token" -> USER_TOKEN;
+                case "api-debounce" -> API_DEBOUNCE;
+                case "account-locked" -> ACCOUNT_LOCKED;
+                case "captcha" -> CAPTCHA;
+                default -> 0;
+            };
+        }
     }
 
+    public static final class REGEX {
+        public static final String EMAIL = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    }
+
+    @Component
     public static final class OTHER {
-        public static final String AUTHORIZATION_PREFIX = "Bearer ";
-        public static final int MAX_PASSWORD_ATTEMPT = 5;
-        public static final List<String> ALLOWED_PATH = List.of(
+        /*
+         * Since 'ExceptionFilter' is not in the filter chain of Security,
+         * if an exception occurs in the filter,
+         * the path forwarded by the 'ExceptionFilter' will pass through the filter chain of Security,
+         * resulting in the failure to throw an exception normally.
+         */
+        public static final List<String> JWT_ALLOWED_PATH = List.of(
                 "/exceptions"
         );
+        public static final String AUTHORIZATION_PREFIX = "Bearer ";
+
+        public static String CAPTCHA_TEMPLATE;
+        public static int LOGIN_FAILURE_LIMIT;
+
+        public OTHER (
+                @Value("${template.html.captcha}") String CAPTCHA_TEMPLATE,
+                @Value("${other.attempt.login-failure-limit}") int LOGIN_FAILURE_LIMIT
+        ) {
+            OTHER.CAPTCHA_TEMPLATE = CAPTCHA_TEMPLATE;
+            OTHER.LOGIN_FAILURE_LIMIT = LOGIN_FAILURE_LIMIT;
+        }
+
+        public static Object get(String key) {
+            return switch (key) {
+                case "login-failure-limit" -> LOGIN_FAILURE_LIMIT;
+                case "captcha" -> CAPTCHA_TEMPLATE;
+                default -> null;
+            };
+        }
     }
 }
