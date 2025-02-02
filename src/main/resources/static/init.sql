638a8f030cb3acdbe610b15d01ea455cb62e2acc
TwAzuremy <958559114@qq.com>
2025-02-02 13:12:00 +0800

    Update sql

--- src/main/resources/static/init.sql
+++ src/main/resources/static/init.sql
@@ -6,5 +6,6 @@ create table user
     username varchar(64)  not null,
     password char(96)     not null,
     email    varchar(255) not null unique,
+    profile  varchar(255) default 'This user does not have a profile.',
     constraint email_format CHECK (email REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
 );
