44723aedc8e2791ea736a7c5a99192d3ed0214a5
TwAzuremy <958559114@qq.com>
2025-02-12 00:08:21 +0800

    Update the database tables

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -3,17 +3,22 @@ create database if not exists simpleLogin;
 create table user
 (
     id       bigint primary key auto_increment,
-    username varchar(64)  null unique,
+    username varchar(64)  not null unique,
     password char(96)     null,
-    email    varchar(255) null unique,
-    profile  varchar(255) null
+    name     varchar(64)  null,
+    email    varchar(255) not null unique,
+    profile  varchar(255) null,
+    constraint email_format CHECK (email REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
 );
 
 create table oauth2_user
 (
-    id          bigint primary key auto_increment,
-    user_id     bigint       not null,
+    id          CHAR(36) PRIMARY KEY DEFAULT UUID(),
+    user_id     bigint       null,
     provider    varchar(255) not null,
     provider_id varchar(255) not null,
+    username    varchar(64)  null unique,
+    email       varchar(255) null,
+    profile     varchar(255) null,
     foreign key (user_id) references user (id)
 );
