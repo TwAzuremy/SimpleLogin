eb36a5403e16103882c46407b1c83a8785d7a908
TwAzuremy <958559114@qq.com>
2025-02-12 19:40:41 +0800

    feat: Add Google Sign-in

--- src/main/java/com/framework/simpleLogin/utils/OAUTH2.java
+++ src/main/java/com/framework/simpleLogin/utils/OAUTH2.java
@@ -10,9 +10,13 @@ public final class OAUTH2 {
     @OAuth2ClientValue(key = "github")
     public static Map<String, String> GITHUB;
 
+    @OAuth2ClientValue(key = "google")
+    public static Map<String, String> GOOGLE;
+
     public static Map<String, String> get(String key) {
         return switch (key) {
             case "github" -> GITHUB;
+            case "google" -> GOOGLE;
             default -> null;
         };
     }
